6b24080ee55f0887f585374260453e45af1178a4
voteDKB <gasdango@ymail.ne.jp>
2026-07-05 23:44:33 +0900

    Add service worker for caching assets
    Implement service worker caching for assets.

--- sw.js
+++ sw.js
@@ -1,1 +1,46 @@
+const CACHE_NAME = "bbhub-v1.3";
 
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// インストール時にキャッシュ
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// 有効化
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+// キャッシュ優先
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
