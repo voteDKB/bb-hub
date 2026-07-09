0853beb3d7e0e4447fcd49cc683e1f85abfc3a35
voteDKB <gasdango@ymail.ne.jp>
2026-07-09 13:33:47 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -36,11 +36,19 @@ self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 
-// キャッシュ優先
+// Network First
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
